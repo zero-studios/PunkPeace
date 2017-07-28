f3fffe2ad3091883f9761bc67627cd7e051e7d2b
jmore88 <jmorel88@yahoo.com>
2017-07-28 11:55:37 -0400

    styles to home page

--- web/app/themes/clean/templates/modules/navigation.php
+++ web/app/themes/clean/templates/modules/navigation.php
@@ -11,5 +11,28 @@ $primary = array(
 	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
 );
 ?>
+<?php
+// get main nav from WordPress
+$secondary = array(
+	'theme_location'  => '',
+	'menu'            => 'Header Menu',
+	'container'       => 'nav',
+	'container_class' => 'group',
+	'container_id'    => 'main-navigation',
+	'menu_class'      => '',
+	'menu_id'         => '',
+	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+);
+?>
 
-<?php wp_nav_menu($primary); ?>
+
+
+<div id="primary-nav" class="group">
+	<div id="nav-one">
+		<?php wp_nav_menu($primary); ?>
+	</div>
+	<div id="logo"><h1><a href="/"><img src='/assets/images/logo_full.png' /></a></h1></div>
+	<div id="nav-two">
+		<?php wp_nav_menu($secondary); ?>
+	</div>
+</div>
